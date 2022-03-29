c6fdf165ab28d467442e0e9f22aee70a0eb4e3f8
Serhat KAYA <serhat.kaya@emakina.com.tr>
2022-03-29 17:57:29 +0300

    create new component expenseList

--- src/components/Expenses/ExpenseList/ExpenseList.js
+++ src/components/Expenses/ExpenseList/ExpenseList.js
@@ -1,7 +1,24 @@
 import './ExpenseList.css';
+import ExpenseItem from '../ExpenseItem/ExpenseItem';
 
-const ExpenseList = () => {
-	return <div></div>;
+const ExpenseList = (props) => {
+	const expensesContent =
+		props.items.length < 1 ? (
+			<p>No expenses found.</p>
+		) : (
+			props.items.map((expense) => {
+				return (
+					<ExpenseItem
+						key={expense.id}
+						title={expense.title}
+						amount={expense.amount}
+						date={expense.date}
+					/>
+				);
+			})
+		);
+
+	return expensesContent;
 };
 
 export default ExpenseList;
--- src/components/Expenses/Expenses/Expenses.js
+++ src/components/Expenses/Expenses/Expenses.js
@@ -1,6 +1,6 @@
 import './Expenses.css';
-import ExpenseItem from '../ExpenseItem/ExpenseItem';
 import ExpenseFilter from '../ExpenseFilter/ExpenseFilter';
+import ExpenseList from '../ExpenseList/ExpenseList';
 import Card from '../../core/Card/Card';
 import React, { useState } from 'react';
 function Expenses(props) {
@@ -15,29 +15,13 @@ function Expenses(props) {
 		(exp) => exp.date.getFullYear() === Number(filteredYear),
 	);
 
-	const expensesContent =
-		filterExpenses.length < 1 ? (
-			<p>No expenses found.</p>
-		) : (
-			filterExpenses.map((expense) => {
-				return (
-					<ExpenseItem
-						key={expense.id}
-						title={expense.title}
-						amount={expense.amount}
-						date={expense.date}
-					/>
-				);
-			})
-		);
-
 	return (
 		<Card className="expenses">
 			<ExpenseFilter
 				onExpenseFilterChanged={onExpenseFilterChanged}
 				selected={filteredYear}
 			></ExpenseFilter>
-			{expensesContent}
+			<ExpenseList items={filterExpenses} />
 		</Card>
 	);
 }
